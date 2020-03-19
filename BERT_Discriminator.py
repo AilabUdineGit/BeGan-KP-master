5b2210ef7230fc17582452325343c727cd7ef7a7
peppe69 <giuslan@gmail.com>
2020-03-19 17:43:43 +0100

    Customizable Bert model class (only for for BertForSequenceClassification)

--- BERT_Discriminator.py
+++ BERT_Discriminator.py
@@ -10,20 +10,14 @@ from transformers import AdamW, \
 
 
 class NetModel(BertPreTrainedModel):
-
+    # class BertForSequenceClassification(BertPreTrainedModel):
     def __init__(self, config):
         super().__init__(config)
         self.num_labels = config.num_labels
-        self.embedding_dim = config.hidden_size
-        self.loss_function = nn.BCEWithLogitsLoss()  # gl
 
         self.bert = BertModel(config)
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
-        # if PARAMS.USE_BLSTM:
-        #     self.bilstm = nn.LSTM(self.embedding_dim, PARAMS.LSTM_SIZE, bidirectional=True)
-        #     self.classifier = nn.Linear(PARAMS.LSTM_SIZE * 2, config.num_labels)
-        # else:
-        self.classifier = nn.Linear(config.hidden_size, config.num_labels)
+        self.classifier = nn.Linear(config.hidden_size, self.config.num_labels)
 
         self.init_weights()
 
@@ -37,7 +31,47 @@ class NetModel(BertPreTrainedModel):
             inputs_embeds=None,
             labels=None,
     ):
-        batch_size = len(input_ids)
+        r"""
+        labels (:obj:`torch.LongTensor` of shape :obj:`(batch_size,)`, `optional`, defaults to :obj:`None`):
+            Labels for computing the sequence classification/regression loss.
+            Indices should be in :obj:`[0, ..., config.num_labels - 1]`.
+            If :obj:`config.num_labels == 1` a regression loss is computed (Mean-Square loss),
+            If :obj:`config.num_labels > 1` a classification loss is computed (Cross-Entropy).
+
+    Returns:
+        :obj:`tuple(torch.FloatTensor)` comprising various elements depending on the configuration (:class:`~transformers.BertConfig`) and inputs:
+        loss (:obj:`torch.FloatTensor` of shape :obj:`(1,)`, `optional`, returned when :obj:`label` is provided):
+            Classification (or regression if config.num_labels==1) loss.
+        logits (:obj:`torch.FloatTensor` of shape :obj:`(batch_size, config.num_labels)`):
+            Classification (or regression if config.num_labels==1) scores (before SoftMax).
+        hidden_states (:obj:`tuple(torch.FloatTensor)`, `optional`, returned when ``config.output_hidden_states=True``):
+            Tuple of :obj:`torch.FloatTensor` (one for the output of the embeddings + one for the output of each layer)
+            of shape :obj:`(batch_size, sequence_length, hidden_size)`.
+
+            Hidden-states of the model at the output of each layer plus the initial embedding outputs.
+        attentions (:obj:`tuple(torch.FloatTensor)`, `optional`, returned when ``config.output_attentions=True``):
+            Tuple of :obj:`torch.FloatTensor` (one for each layer) of shape
+            :obj:`(batch_size, num_heads, sequence_length, sequence_length)`.
+
+            Attentions weights after the attention softmax, used to compute the weighted average in the self-attention
+            heads.
+
+    Examples::
+
+        from transformers import BertTokenizer, BertForSequenceClassification
+        import torch
+
+        tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+        model = BertForSequenceClassification.from_pretrained('bert-base-uncased')
+
+        input_ids = torch.tensor(tokenizer.encode("Hello, my dog is cute", add_special_tokens=True)).unsqueeze(0)  # Batch size 1
+        labels = torch.tensor([1]).unsqueeze(0)  # Batch size 1
+        outputs = model(input_ids, labels=labels)
+
+        loss, logits = outputs[:2]
+
+        """
+
         outputs = self.bert(
             input_ids,
             attention_mask=attention_mask,
@@ -47,21 +81,28 @@ class NetModel(BertPreTrainedModel):
             inputs_embeds=inputs_embeds,
         )
 
-        sequence_output = outputs[0]
+        pooled_output = outputs[1]
 
-        sequence_output = self.dropout(sequence_output)
-        # if PARAMS.USE_BLSTM:
-        #     bilstm_out, hn = self.bilstm(sequence_output)
-        #     logits = self.classifier(bilstm_out)
-        # else:
-        logits = self.classifier(sequence_output)
+        pooled_output = self.dropout(pooled_output)
+        logits = self.classifier(pooled_output)
 
         outputs = (logits,) + outputs[2:]  # add hidden states and attention if they are here
 
-        return outputs  # (loss), scores, (hidden_states), (attentions)
+        if labels is not None:
+            if self.num_labels == 1:
+                #  We are doing regression
+                loss_fct = nn.MSELoss()
+                loss = loss_fct(logits.view(-1), labels.view(-1))
+            else:
+                loss_fct = nn.CrossEntropyLoss()
+                loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+            outputs = (loss,) + outputs
 
-    def evaluated_loss(self, results, labels):
-        return self.loss_function(input=results, target=labels)
+        return outputs  # (loss), logits, (hidden_states), (attentions)
+
+
+# def evaluated_loss(self, results, labels):
+#         return self.loss_function(input=results, target=labels)
 
 
 class NLPModel:
@@ -94,7 +135,7 @@ class NLPModel:
 
 
 NLP_MODELS = {  # models from transformers library
-    'BERT': NLPModel('BERT', BertTokenizer, BertForSequenceClassification, "bert-base-uncased"),  # bert-large-uncased
+    'BERT': NLPModel('BERT', BertTokenizer, BertForSequenceClassification, "bert-base-uncased"),  # bert-large-uncased  gl: BertForSequenceClassification; BertForMultipleChoice
     'XLNet': NLPModel('XLNet', XLNetTokenizer, XLNetModel, "xlnet-large-uncased"),  # bert-base-cased
     'RoBERTa': NLPModel('Roberta', RobertaTokenizer, RobertaModel, "roberta-base"),
     'DistilBERT': NLPModel('DistilBERT', DistilBertTokenizer, DistilBertModel, 'distilbert-base-uncased'),
