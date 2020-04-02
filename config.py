f05773bf5e5e9fc8458e628028fa87da7b9c549c
SaidaSaad <saida@aims.ac.tz>
2020-04-02 22:52:41 +0200

    D_hidden_dim=768, bert_max_lenght=320, bert_labels =1

--- config.py
+++ config.py
@@ -157,7 +157,7 @@ def model_opts(parser):
 
 
 def dis_opts(parser):
-    parser.add_argument('-D_hidden_dim', type=int, default=150,
+    parser.add_argument('-D_hidden_dim', type=int, default=768,
                         help='hidden dim of Discriminators')
     parser.add_argument('-D_layers', type=int, default=2,
                         help='layer of each blstm')
@@ -637,11 +637,11 @@ def interactive_predict_opts(parser):
 def bert_opts(parser):  # gl
     parser.add_argument('-bert_model', type=str, default='BERT',
                         help='Model from HuggingFace transformers (BERT, ALBERT, ... )')
-    parser.add_argument('-bert_labels', type=int, default=2,
+    parser.add_argument('-bert_labels', type=int, default=1,
                         help='Labels for discriminator classification (2 almost always)')
     parser.add_argument('-bert_learning_rate', type=float, default=0.00004,
                         help='Learning rate for AdamW optimizer')
-    parser.add_argument('-bert_max_length', type=int, default=512,
+    parser.add_argument('-bert_max_length', type=int, default=320,
                         help='Max length of Bert input')
     parser.add_argument('-use_bert_discriminator', action="store_true", default=True,
                         help='If is to use bert discriminator')
