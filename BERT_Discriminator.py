289221debeb261cda76fafb8eaf2b19298d9fdb2
peppe69 <giuslan@gmail.com>
2020-04-20 13:05:37 +0200

    Removed methods related to old rewards calculation

--- BERT_Discriminator.py
+++ BERT_Discriminator.py
@@ -37,15 +37,15 @@ class NetModel(BertPreTrainedModel):
         self.bert = BertModel(config)
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
         self.classifier = nn.Linear(config.hidden_size, config.num_labels)
-        self.sigmoid = nn.Sigmoid()
-        # self.devices = device
-        self.MegaRNN = nn.GRU(hidden_dim, 2 * hidden_dim, n_layers)
-        self.Linear = nn.Linear(2 * hidden_dim, 1)
+        # self.sigmoid = nn.Sigmoid()
+        # # self.devices = device
+        # self.MegaRNN = nn.GRU(hidden_dim, 2 * hidden_dim, n_layers)
+        # self.Linear = nn.Linear(2 * hidden_dim, 1)
 
-        # gl: needed for evaluating rewards
-        self.sigmoid = nn.Sigmoid()
-        self.MegaRNN = nn.GRU(hidden_dim, 2 * hidden_dim, n_layers)
-        self.Linear = nn.Linear(2 * hidden_dim, 1)
+        # # gl: needed for evaluating rewards
+        # self.sigmoid = nn.Sigmoid()
+        # self.MegaRNN = nn.GRU(hidden_dim, 2 * hidden_dim, n_layers)
+        # self.Linear = nn.Linear(2 * hidden_dim, 1)
 
         # self.sigmoid = nn.Sigmoid()  # gl
         # self.tanh = nn.Tanh()  # gl
@@ -79,6 +79,7 @@ class NetModel(BertPreTrainedModel):
 
         # gl: new
         sequence_output = outputs[0]
+        # torch.save(sequence_output, 'prova/sequence_output.pt')  # gl saving tensors
         # sequence_output = self.dropout(sequence_output)
         sequence_output = torch.mean(sequence_output, dim=1)
         sequence_output = self.dropout(sequence_output)
@@ -97,6 +98,7 @@ class NetModel(BertPreTrainedModel):
         # logits = self.tanh(logits)
 
         outputs = (logits,) + outputs[2:]  # add hidden states and attention if they are here
+        # torch.save(outputs, 'prova/R_outputs.pt')  # gl saving tensors
 
         if labels is not None:
             if self.num_labels == 1:
@@ -110,120 +112,120 @@ class NetModel(BertPreTrainedModel):
 
         return outputs  # (loss), scores, (hidden_states), (attentions)
 
-    def get_hidden_states(self, input_ids, attention_mask, token_type_ids, labels):
-        position_ids = None
-        head_mask = None
-        inputs_embeds = None
-        h_states_output = self.forward(input_ids,
-                                       attention_mask,
-                                       token_type_ids,
-                                       position_ids,
-                                       head_mask,
-                                       inputs_embeds,
-                                       labels)
-        loss, logits = h_states_output[:2]
-        #   print("input_ids[0]",list(logits[12].size())[0])
-        batch = list(logits[12].size())[0]
-        # a = (input_ids[:, :512] == 102).nonzero()  # Sa: position for all the sep
-        a = (input_ids[:, :input_ids.shape[1]] == 102).nonzero()  # Sa: position for all the sep
-        #    print("a",a)
-        #    print("a",a.shape)
-        t = []
-        t_even = []
-        t_odd = []
-        for i in range(batch * 2):
-            t.append(a[i, 1].item())
-        for x in range(len(t)):
-            if x % 2 == 0:
-                t_even.append(t[x])
-            else:
-                t_odd.append(t[x])
-        t_diff = []
-        zip_object = zip(t_odd, t_even)
-        for t_odd_i, t_even_i in zip_object:
-            t_diff.append(t_odd_i-t_even_i)
-        t_max = max(t_even)
-        t_min = max(t_diff)
-        h_abstract = torch.zeros(batch, t_max, list(logits[12].size())[2])
-        h_kph = torch.zeros(batch, t_min, list(logits[12].size())[2])
-        for i in range(batch):
-            h_abstract[i, :t_even[i], :] = logits[12][i, :t_even[i], :]
-            h_kph[i, :t_odd[i]-t_even[i], :] = logits[12][i, :t_odd[i]-t_even[i], :]
-    # a = (input_ids[:, :512] == 102).nonzero() #Sa: position for all the sep
-    #     t1 = a[0, 1].item() #sa: pick the first sep that divid src from kp fro samp 1
-    #     t1_ = a[1,1].item()
-    #     t2 = a[2, 1].item()# sa:pick the first sep that divid src from kp fro samp 2
-    #     t2_= a[3,1].item()
-    #     h_abstract_f1 = logits[12][0, :t1, :]
-    #     h_kph_f1 = logits[12][0, t1:t1_, :]
-    #     h_abstract_f2 = logits[12][1, :t2, :]
-    #     h_kph_f2 = logits[12][1, t2:t2_, :]
-    #     if t1 >= t2:
-    #         h_abstract = torch.zeros(2,t1,h_abstract_f1.shape[1])
-    #         h_abstract[0, :t1, :] = h_abstract_f1
-    #         h_abstract[1, :t2, :] = h_abstract_f2
-    #         print(h_abstract.shape)
-    #     else:
-    #         h_abstract = torch.zeros(2, t2, h_abstract_f2.shape[1])
-    #         h_abstract[0, :t1, :] = h_abstract_f1
-    #         h_abstract[1, :t2, :] = h_abstract_f2
-    #     if t1_-t1 >= t2_-t2:
-    #         h_kph = torch.zeros(2, t1_-t1, h_kph_f1.shape[1])
-    #         h_kph[0, :t1_-t1, :] = h_kph_f1
-    #         h_kph[1, :t2_-t2, :] = h_kph_f2
-    #     else:
-    #         h_kph = torch.zeros(2, t2_ - t2, h_kph_f2.shape[1])
-    #         h_kph[0, :t1_ - t1, :] = h_kph_f1
-    #         h_kph[1, :t2_ - t2, :] = h_kph_f2
-        return h_abstract, h_kph
+    # def get_hidden_states(self, input_ids, attention_mask, token_type_ids, labels):
+    #     position_ids = None
+    #     head_mask = None
+    #     inputs_embeds = None
+    #     h_states_output = self.forward(input_ids,
+    #                                    attention_mask,
+    #                                    token_type_ids,
+    #                                    position_ids,
+    #                                    head_mask,
+    #                                    inputs_embeds,
+    #                                    labels)
+    #     loss, logits = h_states_output[:2]
+    #     #   print("input_ids[0]",list(logits[12].size())[0])
+    #     batch = list(logits[12].size())[0]
+    #     # a = (input_ids[:, :512] == 102).nonzero()  # Sa: position for all the sep
+    #     a = (input_ids[:, :input_ids.shape[1]] == 102).nonzero()  # Sa: position for all the sep
+    #     #    print("a",a)
+    #     #    print("a",a.shape)
+    #     t = []
+    #     t_even = []
+    #     t_odd = []
+    #     for i in range(batch * 2):
+    #         t.append(a[i, 1].item())
+    #     for x in range(len(t)):
+    #         if x % 2 == 0:
+    #             t_even.append(t[x])
+    #         else:
+    #             t_odd.append(t[x])
+    #     t_diff = []
+    #     zip_object = zip(t_odd, t_even)
+    #     for t_odd_i, t_even_i in zip_object:
+    #         t_diff.append(t_odd_i-t_even_i)
+    #     t_max = max(t_even)
+    #     t_min = max(t_diff)
+    #     h_abstract = torch.zeros(batch, t_max, list(logits[12].size())[2])
+    #     h_kph = torch.zeros(batch, t_min, list(logits[12].size())[2])
+    #     for i in range(batch):
+    #         h_abstract[i, :t_even[i], :] = logits[12][i, :t_even[i], :]
+    #         h_kph[i, :t_odd[i]-t_even[i], :] = logits[12][i, :t_odd[i]-t_even[i], :]
+    # # a = (input_ids[:, :512] == 102).nonzero() #Sa: position for all the sep
+    # #     t1 = a[0, 1].item() #sa: pick the first sep that divid src from kp fro samp 1
+    # #     t1_ = a[1,1].item()
+    # #     t2 = a[2, 1].item()# sa:pick the first sep that divid src from kp fro samp 2
+    # #     t2_= a[3,1].item()
+    # #     h_abstract_f1 = logits[12][0, :t1, :]
+    # #     h_kph_f1 = logits[12][0, t1:t1_, :]
+    # #     h_abstract_f2 = logits[12][1, :t2, :]
+    # #     h_kph_f2 = logits[12][1, t2:t2_, :]
+    # #     if t1 >= t2:
+    # #         h_abstract = torch.zeros(2,t1,h_abstract_f1.shape[1])
+    # #         h_abstract[0, :t1, :] = h_abstract_f1
+    # #         h_abstract[1, :t2, :] = h_abstract_f2
+    # #         print(h_abstract.shape)
+    # #     else:
+    # #         h_abstract = torch.zeros(2, t2, h_abstract_f2.shape[1])
+    # #         h_abstract[0, :t1, :] = h_abstract_f1
+    # #         h_abstract[1, :t2, :] = h_abstract_f2
+    # #     if t1_-t1 >= t2_-t2:
+    # #         h_kph = torch.zeros(2, t1_-t1, h_kph_f1.shape[1])
+    # #         h_kph[0, :t1_-t1, :] = h_kph_f1
+    # #         h_kph[1, :t2_-t2, :] = h_kph_f2
+    # #     else:
+    # #         h_kph = torch.zeros(2, t2_ - t2, h_kph_f2.shape[1])
+    # #         h_kph[0, :t1_ - t1, :] = h_kph_f1
+    # #         h_kph[1, :t2_ - t2, :] = h_kph_f2
+    #     return h_abstract, h_kph
 
     # def evaluated_loss(self, results, labels):
     #    return self.loss_function(input=results, target=labels)
 
-    def Catter(self, kph, rewards, total_len, device):
-        lengths = [len(kp) + 1 for kp in kph]
-        max_len = max(lengths)
-        x = torch.Tensor([])
-        rewards_shape = rewards.repeat(max_len).reshape(-1, rewards.size(0)).t()
-        x = torch.Tensor([])
-        # x = x.to(self.devices)
-        x = x.to(device)
-        for i, keyphrase in enumerate(rewards_shape):
-            # print("keypharse", keyphrase, i)
-            # print("reward", rewards_shape.shape)
-            # print("lengths",lengths)
-            if i <= len(lengths)-1:
-                x = torch.cat((x, keyphrase[:lengths[i]]))
-                # print("x after",x)
-        x = F.pad(input=x, pad=(0, total_len - x.size(0)), mode='constant', value=0)
-        return x
-
-    def calculate_rewards(self, abstract_t, kph_t, start_len, len_list, pred_str_list, total_len, devices, gamma=0.99):
-        start_len = 1
-        # print("abstact shape", abstract_t.shape)
-        abstract_t = torch.mean(abstract_t, dim=1)
-        abstract_t = abstract_t.unsqueeze(1)
-        # print("abstact shape",abstract_t.shape)
-        # print("khp",kph_t.shape)
-        concat_output = torch.cat((abstract_t, kph_t), dim=1)
-        # print("conat_output",concat_output.shape)
-        concat_output = concat_output.permute(1, 0, 2)
-        # print("conat_output",concat_output.shape)
-        device = torch.device("cuda")  # a CUDA device object
-        concat_output = concat_output.to(device)
-        x, hidden = self.MegaRNN(concat_output)
-        output = self.Linear(x)
-        output = output.squeeze(2).t()
-        avg_outputs = self.sigmoid(output)
-        # reward_outputs = torch.Tensor([]).to(self.devices)  # gl
-        reward_outputs = torch.Tensor([]).to(devices)
-        # print('len_list = ', len_list)  # 32 elementi, = batch
-        for i, len_i in enumerate(len_list):
-            avg_outputs[i, start_len:start_len + len_i] = avg_outputs[i, start_len:start_len + len_i]
-            batch_rewards = self.Catter(pred_str_list[i], avg_outputs[i, start_len:start_len + len_i], total_len)
-            reward_outputs = torch.cat((reward_outputs, batch_rewards))  # gl: torch.Size([192]); dtype: torch.float32
-        # print('reward_outputs.size(): ' + str(reward_outputs.size()) + '; dtype: ' + str(reward_outputs.dtype))  # gl
-        return reward_outputs
+    # def Catter(self, kph, rewards, total_len, device):
+    #     lengths = [len(kp) + 1 for kp in kph]
+    #     max_len = max(lengths)
+    #     x = torch.Tensor([])
+    #     rewards_shape = rewards.repeat(max_len).reshape(-1, rewards.size(0)).t()
+    #     x = torch.Tensor([])
+    #     # x = x.to(self.devices)
+    #     x = x.to(device)
+    #     for i, keyphrase in enumerate(rewards_shape):
+    #         # print("keypharse", keyphrase, i)
+    #         # print("reward", rewards_shape.shape)
+    #         # print("lengths",lengths)
+    #         if i <= len(lengths)-1:
+    #             x = torch.cat((x, keyphrase[:lengths[i]]))
+    #             # print("x after",x)
+    #     x = F.pad(input=x, pad=(0, total_len - x.size(0)), mode='constant', value=0)
+    #     return x
+    #
+    # def calculate_rewards(self, abstract_t, kph_t, start_len, len_list, pred_str_list, total_len, devices, gamma=0.99):
+    #     start_len = 1
+    #     # print("abstact shape", abstract_t.shape)
+    #     abstract_t = torch.mean(abstract_t, dim=1)
+    #     abstract_t = abstract_t.unsqueeze(1)
+    #     # print("abstact shape",abstract_t.shape)
+    #     # print("khp",kph_t.shape)
+    #     concat_output = torch.cat((abstract_t, kph_t), dim=1)
+    #     # print("conat_output",concat_output.shape)
+    #     concat_output = concat_output.permute(1, 0, 2)
+    #     # print("conat_output",concat_output.shape)
+    #     device = torch.device("cuda")  # a CUDA device object
+    #     concat_output = concat_output.to(device)
+    #     x, hidden = self.MegaRNN(concat_output)
+    #     output = self.Linear(x)
+    #     output = output.squeeze(2).t()
+    #     avg_outputs = self.sigmoid(output)
+    #     # reward_outputs = torch.Tensor([]).to(self.devices)  # gl
+    #     reward_outputs = torch.Tensor([]).to(devices)
+    #     # print('len_list = ', len_list)  # 32 elementi, = batch
+    #     for i, len_i in enumerate(len_list):
+    #         avg_outputs[i, start_len:start_len + len_i] = avg_outputs[i, start_len:start_len + len_i]
+    #         batch_rewards = self.Catter(pred_str_list[i], avg_outputs[i, start_len:start_len + len_i], total_len)
+    #         reward_outputs = torch.cat((reward_outputs, batch_rewards))  # gl: torch.Size([192]); dtype: torch.float32
+    #     # print('reward_outputs.size(): ' + str(reward_outputs.size()) + '; dtype: ' + str(reward_outputs.dtype))  # gl
+    #     return reward_outputs
 
 
 class NetModelMC(BertPreTrainedModel):
@@ -234,10 +236,10 @@ class NetModelMC(BertPreTrainedModel):
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
         self.classifier = nn.Linear(config.hidden_size, 1)
 
-        # gl: needed for evaluating rewards
-        self.sigmoid = nn.Sigmoid()
-        self.MegaRNN = nn.GRU(hidden_dim, 2 * hidden_dim, n_layers)
-        self.Linear = nn.Linear(2 * hidden_dim, 1)
+        # # gl: needed for evaluating rewards
+        # self.sigmoid = nn.Sigmoid()
+        # self.MegaRNN = nn.GRU(hidden_dim, 2 * hidden_dim, n_layers)
+        # self.Linear = nn.Linear(2 * hidden_dim, 1)
 
         self.init_weights()
 
